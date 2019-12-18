893323fbff103cd70672aaf9e906df75423e5ed8
Michael Lynch <michael.lynch@ocado.com>
2019-12-18 11:17:57 +0000

    Moved shbang files to test directory

--- config/java_replr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed '1d' $1 | replr /usr/local/share/replr/java.replr
--- test/JavaReplr.java
+++ test/JavaReplr.java
@@ -1,11 +1,11 @@
-#!/usr/bin/java.replr
-<%%>
+public class JavaReplr {
 public static void doSomething() {
     System.out.println("This is an existing method");
 }
-<%%>
+public static void main(String... args) {
 System.out.println("HELLO WORLD");
 System.out.println("This is java from shbang");
-var a = 1+1;
+int a = 1+1;
 System.out.println(a);
 doSomething();
+}}
